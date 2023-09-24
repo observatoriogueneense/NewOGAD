c45def9b9e98f8fad636ffe497329e54e1b5aefc
marianovungel <vungem88@gmail.com>
2023-09-24 09:17:26 -0300

    all

--- src/AdmScreens/api.js
+++ src/AdmScreens/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://backunilabtem.herokuapp.com/',
-    baseURL: 'http://localhost:8000/'
+    baseURL: 'https://backunilabtem.herokuapp.com/',
+    // baseURL: 'http://localhost:8000/'
 });
 
 export default api;
